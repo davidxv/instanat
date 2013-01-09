97634287d1c849d669fe0abf2453d1efc6156bbd
ntlk <natalia.buckley@gmail.com>
2013-01-09 23:28:00 +0000

    apparently this is correct ;)

--- config/database.rb
+++ config/database.rb
@@ -22,9 +22,15 @@ ActiveRecord::Base.configurations[:development] = {
   :port      => 5432
 }
 
+postgres = URI.parse(ENV['DATABASE_URL'] || '')
+
 ActiveRecord::Base.configurations[:production] = {
-  :adapter   => 'postgresql',
-  :database  => 'instanat'
+  :adapter  => 'postgresql',
+  :encoding => 'utf8',
+  :database => postgres.path[1..-1], 
+  :username => postgres.user,
+  :password => postgres.password,
+  :host     => postgres.host
 }
 
 # Setup our logger
