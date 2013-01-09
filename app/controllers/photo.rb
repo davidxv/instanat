15e00160bb106387bf7f5701b6ba2a2aa88106d2
ntlk <natalia.buckley@gmail.com>
2013-01-09 17:54:51 +0000

    tidying up

--- app/controllers/photo.rb
+++ app/controllers/photo.rb
@@ -1,27 +1,10 @@
 Instanat.controllers :photo do
-  # get :index, :map => "/foo/bar" do
-  #   session[:foo] = "bar"
-  #   render 'index'
-  # end
-
-  # get :sample, :map => "/sample/url", :provides => [:any, :js] do
-  #   case content_type
-  #     when :js then ...
-  #     else ...
-  # end
-
-  # get :foo, :with => :id do
-  #   "Maps to url '/foo/#{params[:id]}'"
-  # end
-
-  # get "/example" do
-  #   "Hello world!"
-  # end
 
   get :index do
   end
 
   get :latest, :map => "/" do
+    @photos = Photo.order("modified_at DESC").limit(Instanat.settings.photos_to_display)
     render 'photo/latest'
   end
 
